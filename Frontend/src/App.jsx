5001043565d85e2991bef3b2da51dcd2aa2fa68e
K Mohith Goud <128036418+Mohith202@users.noreply.github.com>
2024-07-24 22:26:55 +0530

    updated package.json

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState,useEffect } from 'react'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 import './App.css'
 
 import Login from './components/login/login';
